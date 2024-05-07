c4fcc21a02b9bdc1d177e3bac00c322417f88ebb
Ameya-Joshi79 <45701553+Ameya-Joshi79@users.noreply.github.com>
2024-05-07 14:31:41 -0400

    App.kt updated
    Kotlin's map() higher Order Function examples. We're also using a little bit of chaining to get the desired result
    
    For E.g. Return all Items that are of Category Vegetables and half price them.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -10,6 +10,45 @@ fun main(args:Array<String>){
         Grocery2("Olive Oil", "Pantry", "Bottle", 6.0, 1),
         Grocery2("Ice Cream", "Frozen", "Pack", 3.0, 2)
     )
-    
+
+    val intList = listOf<Int>(1, 2, 3, 4, 5)
+
+    val timesTwoInts = intList.map { it * 2 }
+
+    val stringInts = intList.map { it.toString() }
+
+    println(stringInts[0])
+
+    println(intList)
+
+    println(timesTwoInts)
+
+    /*
+      Return a list of all items prices (Double) with price reduced to half.
+     */
+    val halfPriced = groceries2.map {
+        it.unitPrice * 0.5
+    }
+
+
+    /*
+        Return all Items that are of Category Vegetables and half price them. We're chaining the higher order functions.
+        Note- filterNotNull returns a list of all items in a collection that are not null
+     */
+
+    val halfPricedVegetableItems = groceries2.map {
+
+        var item:Grocery2? = null
+
+        if (it.category == "Vegetable") {
+
+            item = it.copy(unitPrice = it.unitPrice * 0.5)
+
+        }
+
+        item
+    }.filterNotNull()
+
+    println(halfPricedVegetableItems)
 
 }
